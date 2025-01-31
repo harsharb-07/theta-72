7c718884fbda1eb400961e705da9f94d46725d9e
harsharb-07 <167626145+harsharb-07@users.noreply.github.com>
2025-01-31 17:34:41 +0000

    LinearRegression of the data

---
+++ LinearRegression.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from google.colab import files
+uploaded = files.upload()
+file_name = list(uploaded.keys())[0]
+df = pd.read_csv(file_name)
+print(df.head())
+df
+
+df.shape
+
+import seaborn as sns
+sns.distplot(df['daily'])
