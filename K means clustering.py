8729218071fcef4cf60a2c9f533768ec8ab9fd06
harsharb-07 <167626145+harsharb-07@users.noreply.github.com>
2025-02-14 04:20:33 +0000

    K means clustering

---
+++ K means clustering.py
@@ -0,0 +1,43 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+%matplotlib inline
+from google.colab import drive
+drive.mount('/content/drive')
+import pandas as pd
+file_path = "/content/income.csv"
+df = pd.read_csv(file_path)
+df = pd.read_csv("income.csv")
+df.head()
+plt.scatter(df.Age,df['Income($)'])
+plt.xlabel('Age')
+plt.ylabel('Income ($)')km = KMeans(n_clusters=3)
+y_predicted = km.fit_predict(df[['Age','Income($)']])
+y_predicted
+df['cluster'] = y_predicted
+df.head()
+km.cluster_centers_
+km = KMeans(n_clusters=3)
+df['cluster'] = km.fit_predict(df[['Age','Income($)']])
+
+df1 = df[df.cluster==0]
+df2 = df[df.cluster==1]
+df3 = df[df.cluster==2]
+
+plt.scatter(df1.Age,df1['Income($)'],color='green')
+plt.scatter(df2.Age,df2['Income($)'],color='red')
+plt.scatter(df3.Age,df3['Income($)'],color='black')
+
+plt.scatter(km.cluster_centers_[:,0],km.cluster_centers_[:,1],color='purple',marker='*',label='centroid')
+
+plt.xlabel('Age')
+plt.ylabel('Income ($)')
+plt.legend()
+
+scaler = MinMaxScaler()
+scaler.fit(df[['Income($)']])
+df['Income($)'] = scaler.transform(df[['Income($)']])
+
+scaler.fit(df[['Age']])
+df['Age'] = scaler.transform(df[['Age']])
